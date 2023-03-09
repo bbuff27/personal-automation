d22f8c4961d396f4f2d156731e4391962743a0e6
Bill Buff <bbuff27@gmail.com>
2023-03-08 21:44:45 -0700

    Set up expense verification prompt

--- expenses/calculateExpenses.js
+++ expenses/calculateExpenses.js
@@ -9,48 +9,36 @@ dotenv.config();
 
 const directoryPath = process.env.DOWNLOADS_PATH;
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
 fs.readdir(directoryPath, (err, files) => {
   if (err) {
     console.error(err);
     return;
   }
 
-  const expenseFiles = files
-    .filter(file => file.includes('_Expenses') && path.extname(file) === '.csv')
+  const calculatedExpenses = files.filter(file => file.includes('_Expenses') && path.extname(file) === '.csv')
     .reduce((acc, file) => {
       const filePath = path.join(directoryPath, file);
       const headers = ['description', 'amount', 'category'];
+      const csvStream = fs.createReadStream(filePath).pipe(csv({ headers }));
 
-      fs.createReadStream(filePath)
-        .pipe(csv({ headers }))
+      csvStream
         .on('data', ({ description, amount, category }) => {
-          const expense = {
-            description,
-            amount,
-            category,
+          if(description !== 'Description') {
+            csvStream.pause();
+            rl.question(`Choose an expense category for ${description} for ${amount} currently under ${category} category: `, (answer) => {
+              const [parentCategory, subCategory] = answer.split('.');
+              const selectedItem = categories[parentCategory][subCategory];
+              console.log('You selected:', selectedItem);
+              rl.close();
+              csvStream.resume();
+            });
           }
-        })
-        .on('data', (expense) => {
-          const rl = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout
-          });
-          let i = 1;
-          for (const [key, value] of Object.entries(expense)) {
-            console.log(`${i}. ${key}`);
-            for (const [childKey, childValue] of Object.entries(value)) {
-              console.log(`${i}.${Object.keys(value).indexOf(childKey) + 1}. ${childKey}: ${childValue}`);
-            }
-            i++;
-          }
-
-          rl.question('Enter your selection: ', (answer) => {
-            const selection = parseInt(answer, 10);
-            const selectedItem = Object.values(expense)[selection - 1];
-            console.log('You selected:', selectedItem);
-            rl.close();
-          });
         });
-      }, categories)
+    }, categories)
   // console.log(expenseFiles)
 });
