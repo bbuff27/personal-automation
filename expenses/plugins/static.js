11a41826637448e19ce8af5c68a090c2a6e095b3
Bill Buff <bbuff27@gmail.com>
2023-03-12 21:36:28 -0600

    Get styles to work

---
+++ expenses/plugins/static.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const path = require('path');
+
+const fp = require('fastify-plugin');
+
+module.exports = fp(async function (fastify, opts) {
+  fastify.register(require('@fastify/static'), {
+    root: path.join(__dirname, '../styles'),
+    prefix: '/styles/'
+  });
+})
