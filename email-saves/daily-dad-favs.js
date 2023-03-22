4fbd1094788e1889098291adad6dd1663e224a31
Bill Buff <bbuff27@gmail.com>
2023-03-21 21:43:15 -0600

    Add packages and configure environment variavbles

--- email-saves/daily-dad-favs.js
+++ email-saves/daily-dad-favs.js
@@ -1,23 +1,26 @@
 const ProtonMailClient = require('protonmail-api');
 const { Client } = require('@notionhq/client');
-const notion = new Client({ auth: 'YOUR_NOTION_API_KEY' });
+require('dotenv').config();
+
+const notion = new Client({ auth: process.env.NOTION_API_KEY });
 
 // Initialize ProtonMail client
 const protonmail = new ProtonMailClient({
-  username: 'YOUR_PROTONMAIL_USERNAME',
-  password: 'YOUR_PROTONMAIL_PASSWORD',
+  username: process.env.PROTON_USERNAME,
+  password: process.env.PROTON_PASSWORD,
   baseUrl: 'https://mail.protonmail.com'
 });
 
 // Get the database ID of your Notion database
-const databaseId = 'YOUR_DATABASE_ID';
+const databaseId = process.env.NOTION_DATABASE_ID;
+
 
 // Get a list of new messages with a specific label from ProtonMail
 protonmail
   .login()
   .then(() => {
     return protonmail.getMessages({
-      label: 'YOUR_LABEL_NAME'
+      label: process.env.LABEL_NAME
     });
   })
   .then(messages => {
@@ -25,20 +28,11 @@ protonmail
     messages.forEach(message => {
       const properties = {
         // Add your own properties and formatting here
-        'Name': {
+        'Subject': {
           title: [
             {
               text: {
                 content: message.Subject
-              }
-            }
-          ]
-        },
-        'From': {
-          rich_text: [
-            {
-              text: {
-                content: message.From[0].Name
               }
             }
           ]
@@ -68,12 +62,12 @@ protonmail
       });
 
       // Mark the message as read and move it to the Trash folder in ProtonMail
-      protonmail.updateMessage({
-        id: message.ID,
-        unread: 0,
-        labelIDsAdded: [protonmail.labelIDs.TRASH],
-        labelIDsRemoved: [protonmail.labelIDs.INBOX, protonmail.labelIDs.IMPORTANT]
-      });
+      // protonmail.updateMessage({
+      //   id: message.ID,
+      //   unread: 0,
+      //   labelIDsAdded: [protonmail.labelIDs.TRASH],
+      //   labelIDsRemoved: [protonmail.labelIDs.INBOX, protonmail.labelIDs.IMPORTANT]
+      // });
     });
   })
   .catch(error => {
