1d2456eb1d251fcf876231b28f4b2eab88e37f0e
Bill Buff <bbuff27@gmail.com>
2023-03-23 16:28:42 -0600

    Waiting for package to get updated

--- email-saves/daily-dad-favs.js
+++ email-saves/daily-dad-favs.js
@@ -1,76 +1,61 @@
-const ProtonMailClient = require('protonmail-api');
+const ProtonMail = require('protonmail-api');
 const { Client } = require('@notionhq/client');
 require('dotenv').config();
 
-const notion = new Client({ auth: process.env.NOTION_API_KEY });
+(async () => {
+  const notion = new Client({ auth: process.env.NOTION_API_KEY });
+  try {
+    const protonmail = await ProtonMail.connect({
+      username: process.env.PROTON_USERNAME,
+      password: process.env.PROTON_PASSWORD,
+      puppeteerOpts: {
+        headless: false
+      }
+    })
 
-// Initialize ProtonMail client
-const protonmail = new ProtonMailClient({
-  username: process.env.PROTON_USERNAME,
-  password: process.env.PROTON_PASSWORD,
-  baseUrl: 'https://mail.protonmail.com'
-});
-
-// Get the database ID of your Notion database
-const databaseId = process.env.NOTION_DATABASE_ID;
-
-
-// Get a list of new messages with a specific label from ProtonMail
-protonmail
-  .login()
-  .then(() => {
-    return protonmail.getMessages({
-      label: process.env.LABEL_NAME
-    });
-  })
-  .then(messages => {
-    // Iterate over each message and push it to Notion
-    messages.forEach(message => {
+    const emails = await protonmail.getEmails({ label: process.env.LABEL_NAME })
+    for (const email of emails) {
       const properties = {
-        // Add your own properties and formatting here
         'Subject': {
           title: [
             {
               text: {
-                content: message.Subject
+                content: email.subject
               }
             }
           ]
         },
         'Date': {
           date: {
-            start: message.Time * 1000 // Convert Unix timestamp to milliseconds
+            start: email.time
           }
         },
         'Body': {
           rich_text: [
             {
               text: {
-                content: message.Body
+                content: email.getBody()
               }
             }
           ]
         }
       };
+    }
 
-      // Push the message to your Notion database
-      notion.pages.create({
-        parent: {
-          database_id: databaseId
-        },
-        properties: properties
-      });
+    console.log({ email, properties });
 
-      // Mark the message as read and move it to the Trash folder in ProtonMail
-      // protonmail.updateMessage({
-      //   id: message.ID,
-      //   unread: 0,
-      //   labelIDsAdded: [protonmail.labelIDs.TRASH],
-      //   labelIDsRemoved: [protonmail.labelIDs.INBOX, protonmail.labelIDs.IMPORTANT]
-      // });
-    });
-  })
-  .catch(error => {
-    console.error(error);
-  });
+    // Push the email to your Notion database
+    // notion.pages.create({
+    //   parent: {
+    //     database_id: process.env.NOTION_DATABASE_ID,
+    //   },
+    //   properties,
+    // });
 
+    // Mark the email as read and move it to the Trash folder in ProtonMail
+    // email.delete();
+    protonmail.close();
+  } catch (error) {
+    console.error(error)
+  }
+})()
